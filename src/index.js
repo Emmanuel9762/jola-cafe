55c5573d48f07d93ffc29f061578576e788ba3f5
Emmanuel <nlihleemmanuel@outlook.com>
2024-09-16 14:54:06 +0200

    1st commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function carousel() {
+    return {
+        currentIndex: 0,
+        images: [
+            '../assets/hero.jpg',
+            '../assets/jola-facade.jpg',
+            '../assets/jola-drone.jpg'
+        ],
+        init() {
+            setInterval(() => {
+                this.currentIndex = (this.currentIndex + 1) % this.images.length;
+            }, 5000);
+        }
+    }
+}
